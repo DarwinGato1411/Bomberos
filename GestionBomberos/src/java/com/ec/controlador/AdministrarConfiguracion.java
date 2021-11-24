6e88f8a2b2111557c25a493f904b05b891754d2a
DarwinGato1411 <darwinvinicio14@gmail.com>
2021-11-24 01:28:41 -0500

    danny parametrizar

--- GestionBomberos/src/java/com/ec/controlador/AdministrarConfiguracion.java
+++ GestionBomberos/src/java/com/ec/controlador/AdministrarConfiguracion.java
@@ -4,17 +4,35 @@
  */
 package com.ec.controlador;
 
+import com.ec.entidad.Opciones;
+import com.ec.entidad.Parametrizar;
 import com.ec.entidad.Perfil;
 import com.ec.entidad.Usuario;
+import com.ec.seguridad.UserCredential;
+import com.ec.servicio.ServicioParametrizar;
 import com.ec.servicio.ServicioPerfil;
 import com.ec.servicio.ServicioUsuario;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.io.Files;
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Fileupload;
+import org.zkoss.zul.Image;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Window;
 
 /**
  *
@@ -22,12 +40,29 @@ import org.zkoss.zk.ui.Executions;
  */
 public class AdministrarConfiguracion {
 
+    @Wire
+    Window wParametrizar;
     ServicioPerfil servicioPerfil = new ServicioPerfil();
     ServicioUsuario servicioUsuario = new ServicioUsuario();
+    ServicioParametrizar servicioParametrizar = new ServicioParametrizar();
     private List<Usuario> listaUsuarios = new ArrayList<Usuario>();
     private List<Perfil> listaPerfil = new ArrayList<Perfil>();
+    private Parametrizar parametrizarselected = new Parametrizar();
+    private List<String> listaDicos = new ArrayList<String>();
+    private String tipoAccion = "new";
+
+    UserCredential credential = new UserCredential();
 
     public AdministrarConfiguracion() {
+//        Selectors.wireComponents(view, this, false);
+//        if (valor != null) {
+//            tipoAccion = "update";
+//            parametrizarselected = valor;
+//            
+//        } else {
+//            tipoAccion = "new";
+//            parametrizarselected = new Parametrizar();
+//        }
         cosultarUsuarios("");
         cosultarPerfiles("");
     }
@@ -92,12 +127,135 @@ public class AdministrarConfiguracion {
         cosultarUsuarios("");
     }
 
+    private void listaDiscos() {
+        File[] files = File.listRoots();
+        if (files != null) {
+            for (File f : files) {
+                listaDicos.add(f.getPath());
+                System.out.println("getAbsolutePath " + f.getAbsolutePath());
+                System.out.println("getPath " + f.getPath());
+            }
+        }
+
+    }
+    //Imagen ruta 
+    private String filePathImg;
+
+    @Command
+    @NotifyChange({"fileContent", "tipoambiente"})
+    public void subirPathImagen() throws InterruptedException, IOException {
+
+        org.zkoss.util.media.Media media = Fileupload.get();
+        if (media instanceof org.zkoss.image.Image) {
+            String nombre = media.getName();
+            if (media instanceof Image) {
+
+                if (media.getByteData().length > 10 * 1024 * 1024) {
+                    Messagebox.show("El arhivo seleccionado sobrepasa el tamaño de 10Mb.\n Por favor seleccione un archivo más pequeño.", "Atención", Messagebox.OK, Messagebox.ERROR);
+
+                    return;
+                }
+                filePathImg = parametrizarselected.getParCarpeta() + File.separator + parametrizarselected.getParFolderAdicional() + File.separator;
+
+                File baseDir = new File(filePathImg);
+                if (!baseDir.exists()) {
+                    baseDir.mkdirs();
+                }
+                Files.copy(new File(filePathImg + media.getName()),
+                        media.getStreamData());
+                parametrizarselected.setParCarpeta(filePathImg + File.separator + nombre);
+            }
+
+        }
+    }
+
+    @Command
+    @NotifyChange("parametrizarselected")
+    public void guardar() {
+        if (parametrizarselected != null && parametrizarselected.getParEmpresa() != null
+                && parametrizarselected.getParRucEmpresa() != null) {
+            if (tipoAccion.equals("new")) {
+                servicioParametrizar.crear(parametrizarselected);
+            } else {
+                servicioParametrizar.modificar(parametrizarselected);
+            }
+            wParametrizar.detach();
+        } else {
+            Clients.showNotification("Verifique la informacion... ",
+                    Clients.NOTIFICATION_TYPE_ERROR, null, "end_center", 3000, true);
+        }
+    }
+
     public List<Perfil> getListaPerfil() {
         return listaPerfil;
     }
 
     public void setListaPerfil(List<Perfil> listaPerfil) {
         this.listaPerfil = listaPerfil;
+    }
+
+    public Parametrizar getParametrizarselected() {
+        return parametrizarselected;
+    }
+
+    public void setParametrizarselected(Parametrizar parametrizarselected) {
+        this.parametrizarselected = parametrizarselected;
+    }
+
+    public List<String> getListaDicos() {
+        return listaDicos;
+    }
+
+    public void setListaDicos(List<String> listaDicos) {
+        this.listaDicos = listaDicos;
+    }
+
+    public Window getwParametrizar() {
+        return wParametrizar;
+    }
+
+    public void setwParametrizar(Window wParametrizar) {
+        this.wParametrizar = wParametrizar;
+    }
+
+    public ServicioUsuario getServicioUsuario() {
+        return servicioUsuario;
+    }
+
+    public void setServicioUsuario(ServicioUsuario servicioUsuario) {
+        this.servicioUsuario = servicioUsuario;
+    }
+
+    public ServicioParametrizar getServicioParametrizar() {
+        return servicioParametrizar;
+    }
+
+    public void setServicioParametrizar(ServicioParametrizar servicioParametrizar) {
+        this.servicioParametrizar = servicioParametrizar;
+    }
+
+    public String getTipoAccion() {
+        return tipoAccion;
+    }
+
+    public void setTipoAccion(String tipoAccion) {
+        this.tipoAccion = tipoAccion;
+    }
+
+    public UserCredential getCredential() {
+        return credential;
+    }
+
+    public void setCredential(UserCredential credential) {
+        this.credential = credential;
+    }
+
+    public String getFilePathImg() {
+        return filePathImg;
+    }
+
+    public void setFilePathImg(String filePathImg) {
+        this.filePathImg = filePathImg;
     }
 
 }
--- GestionBomberos/src/java/com/ec/entidad/Parametrizar.java
+++ GestionBomberos/src/java/com/ec/entidad/Parametrizar.java
@@ -289,4 +289,8 @@ public class Parametrizar implements Serializable {
         return "com.ec.entidad.Parametrizar[ codParametrizar=" + codParametrizar + " ]";
     }
 
+    public void setIdUsuario(Usuario usuarioSistema) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
